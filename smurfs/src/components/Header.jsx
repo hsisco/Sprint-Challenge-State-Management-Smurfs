c8c80933b636999a27f9cd2052d1b291095350ee
hsisco <hysensisco@gmail.com>
2019-11-08 08:43:45 -0800

    Update Header.jsx

---
+++ smurfs/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+  <div className="header">
+    <h1>SMURFS!</h1>
+    <div>Socialist Utopia or Totalitarian Racists??</div>
+  </div>);
+};
+
+export default Header;
