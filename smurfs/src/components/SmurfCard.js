ea3e42ac032cdd5e450a553945dbfa0b2de44b80
hsisco <hysensisco@gmail.com>
2020-02-09 18:34:32 -0800

    SmurfCard + boostrap

---
+++ smurfs/src/components/SmurfCard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+
+const SmurfCard = (props) => {
+  // console.log(props.deleteSmurf);
+  const handleClick = e => {
+    props.deleteSmurf({ id: e.target.value})
+    console.log("e", e.target.value)
+  }
+
+  return (
+      <Card className="card" style={{ width: '18rem' }}>
+        <Card.Body>
+          <Card.Title>Name: {props.data.name}</Card.Title>
+          <Card.Text>
+            Age: {props.data.age}<br/>
+            Height: {props.data.height}<br/>
+          </Card.Text>
+          <button value={props.data.id} onClick={handleClick}>Delete</button>
+        </Card.Body>
+      </Card>
+    )
+};
+
+export default SmurfCard
