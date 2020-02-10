77b1e9f4f01eccaa9e4bb03c7a5a77c5c4093a27
hsisco <hysensisco@gmail.com>
2020-02-09 18:42:46 -0800

    Add thunk

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -6,7 +6,7 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import reducer from './reducer';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 const store = createStore(reducer, applyMiddleware(thunk));
 
